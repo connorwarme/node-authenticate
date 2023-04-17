cd93596990a9bf7766aaa11ef2c0f1e4603e3ce3
connorwarme <connor.warme@gmail.com>
2023-04-16 19:27:59 -0600

    Initial setup for lesson on authentication

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config() 
+const express = require("express")
+const path = require("path")
+const session = require("express-session")
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const mongoDB = process.env.MONGO
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "mongo connection error"))
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    }
+  })
+)
+
+const app = express();
+app.set("views", __dirname)
+app.set("view engine", "ejs")
+
+app.use(session({ secret: "rocks", resave: false, saveUninitialized: true }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => res.render("index"))
+
+app.listen(3000, () => console.log("app listening on local, port 3000!"))
