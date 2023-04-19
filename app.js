b692c0a20655ec021cb4aefdeae7586df07284b8
connorwarme <connor.warme@gmail.com>
2023-04-18 18:22:39 -0600

    Add bcrypt, test, operational

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express")
 const path = require("path")
 const session = require("express-session")
+const bcrypt = require("bcryptjs")
 const passport = require("passport")
 const LocalStrategy = require("passport-local").Strategy
 const mongoose = require("mongoose")
@@ -32,10 +33,13 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" })
       }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" })
-      }
-      return done(null, user)
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user)
+        } else {
+          return done(null, false, { message: "Incorrect password" })
+        }
+      })
     } catch (err) {
       return done(err)
     }
@@ -68,17 +72,21 @@ app.get("/", (req, res) => {
 app.get("/sign-up", (req, res) => res.render("sign-up"))
 app.post("/sign-up", async (req, res, next) => {
   try {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
+    bcrypt.hash(req.body.password, 12, async (err, hashedPassword) => {
+      if (err) {
+        return next(err)
+      }
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      })
+      const result = await user.save()
+      res.redirect("/"); 
     })
-    const result = await user.save()
-    res.redirect("/"); 
   } catch(err) {
       return next(err)
-    }
   }
-)
+})
 app.post("/log-in", 
   passport.authenticate("local", {
     successRedirect: "/",
